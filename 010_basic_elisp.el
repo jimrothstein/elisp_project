b21726b80a03e262cfb31c04e9bd4e170b54e4c1
jimrothstein <jimrothstein@gmail.com>
2024-09-18 17:39:04 -0700

    wip

---
+++ 010_basic_elisp.el
@@ -0,0 +1,19 @@
+
+;; To run:   M-x ielm RET
+;; To run buffer:   M-x eval-buffer
+;; To run ONE line;   M-x eval-region
+
+;; **Position AFTER code; C-x C-e evaluates**
+
+(+ 4 5)
+
+emacs-version
+buffer-file-name
+
+;; function
+(defun add-nums (+ a b) )
+(add-nums 2 3)
+
+
+
+(setq a 'x)
