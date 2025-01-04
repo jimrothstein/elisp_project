c3119afc7294253e2ef8ddb5fa17eab32ac43cc8
jimrothstein <jimrothstein@gmail.com>
2025-01-03 18:02:52 -0800

    wip

---
+++ 110_basic_evil_config.el
@@ -0,0 +1,17 @@
+;;; package --- Summary
+
+;; C-x C-e to run SEXP
+
+;; normal cursor = purple
+(setq evil-normal-state-cursor '(box "purple"))
+
+;; insert cursor = bar, greenish 
+(setq evil-insert-state-cursor '((bar . 5)  "green"))
+
+
+
+
+
+
+
+
