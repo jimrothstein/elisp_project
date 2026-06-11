57ff82b5d28b9316dccb7516c10ebc8f03628e5f
jimrothstein <jimrothstein@gmail.com>
2026-06-11 16:31:21 -0700

    wip

---
+++ 610_macros.el
@@ -0,0 +1,31 @@
+
+f̱oo bar baz quux
+foo bar baz quux
+foo bar baz quux
+foo bar baz quux
+
+
+macro: begin, capitalize word, down 1 line, end
+C-x ( M-u C-n C-x )
+C-x ( M-u C-n C-x )
+
+SALLY
+(defalias 'jim
+   (kmacro "M-u"))
+
+(defalias 'jim
+   (kmacro "M-u"))
+
+
+(defalias 'jim
+   (kmacro "M-u"))
+(defalias 'jim
+   (kmacro "M-u"))
+
+
+
+
+
+
+
+
